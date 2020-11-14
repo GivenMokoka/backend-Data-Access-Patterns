070852ceb32efb0445880919868081ab5b56edfb
Francisco Jose Fernandez Garcia <franjfgcarmo@gmail.com>
2020-11-14 11:15:03 +0100

    feat: Ignored property

--- src/MyShop.Infrastructure/ShoppingContext.cs
+++ src/MyShop.Infrastructure/ShoppingContext.cs
@@ -18,5 +18,11 @@ namespace MyShop.Infrastructure
                 // .UseLazyLoadingProxies()
                 .UseSqlite("Data Source=orders.db");
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .Ignore(c => c.ProfilePicture);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
